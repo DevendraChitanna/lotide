ad06a9c858350fc6f8f91193f54c326b9298c595
DevendraChitanna <devendrachitanna@gmail.com>
2021-11-24 00:24:44 -0500

    created assertArraysEqual

---
+++ assertArraysEqual.js
@@ -0,0 +1,26 @@
+const eqArrays = function (actual, expected) {
+
+  if (actual.length !== expected.length) {
+    return false
+  }
+
+  for (let i = 0; i < actual.length; i++) {
+    if (actual[i] !== expected[i]) {
+      return false
+    }
+  }
+  return true;
+}
+
+//*********************************************************************** */
+
+const assertArraysEqual = function (actual, expected) {
+  if (eqArrays(actual, expected) === true) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
+  }
+}
+
+console.log(assertArraysEqual([1, 2, 3], [1, 2, 3]))
+//where is undefined coming from???
