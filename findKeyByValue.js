faa34250c6c32d9db2ef2a41fea9a73750678b55
DevendraChitanna <devendrachitanna@gmail.com>
2021-11-24 22:12:51 -0500

    compleated findKeyByValue

---
+++ findKeyByValue.js
@@ -0,0 +1,30 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
+  };
+};
+
+//*************************************************************************** */
+const findKeyByValue = function (obj, find) {
+  for (let genre in obj) {
+    // console.log("genre", obj[genre])
+    if (obj[genre] === find) {
+      // console.log("found")
+      return genre
+    }
+  }
+}
+
+
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire"
+};
+// console.log(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"))
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
--- tail.js
+++ tail.js
@@ -1,15 +1,15 @@
-const assertEqual = function(actual, expected) {
+const assertEqual = function (actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
   } else (console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`));
 };
 
 //***************************************************************************** */
-const tail = function(array) {
+const tail = function (array) {
   let newArray;
   if (array.length <= 1) {
     return [];
-  }  else {
+  } else {
     newArray = array.slice(1);
     return newArray;
   }
@@ -19,5 +19,4 @@ const tail = function(array) {
 // Test Case: Check the original array
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 tail(words); // no need to capture the return value since we are not checking it
-console.log(words)
 assertEqual(words.length, 3); // original array should still have 3 elements!
