201e790b4583d099a5dcf4657f6dab1952d72ac5
DevendraChitanna <devendrachitanna@gmail.com>
2021-11-25 14:35:19 -0500

    completed assertObjectsEqual

--- eqObjects.js
+++ eqObjects.js
@@ -27,20 +27,18 @@ const eqArrays = function (actual, expected) {
 // Returns true if both objects have identical keys with identical values.
 // Otherwise you get back a big fat false!
 const eqObjects = function (object1, object2) {
-  // console.log(Object.keys(object1).length)
   if (Object.keys(object1).length !== Object.keys(object2).length) {
     return false;
   }
   for (let key in object1) {
-
-    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
+    if (Array.isArray(object1[key]) === Array.isArray(object2[key])) {
       if (!eqArrays(object1[key], object2[key])) {
-        return false;
+        return false
       }
     } else if (object1[key] !== object2[key]) {
       return false;
-
     }
+
 
   }
   return true;
