fda33a4663843d6812b4659f44992d00c3797479
DevendraChitanna <devendrachitanna@gmail.com>
2021-11-25 20:42:28 -0500

    complete findKey

---
+++ findKey.js
@@ -0,0 +1,32 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
+  };
+};
+//**************************************************************************** */
+
+const findKey = function (obj, call) {
+  //scan obj and return the first key that the make the callback true
+  // let keys = Object.keys(obj)
+  // console.log(keys)
+  for (let val in obj) {
+    // console.log(obj[val])
+    if (call(obj[val])) {
+      return val
+    }
+  }
+  return undefined
+}
+
+
+
+console.log(assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri": { stars: 3 },
+  "noma": { stars: 2 },
+  "elBulli": { stars: 3 },
+  "Ora": { stars: 2 },
+  "Akelarre": { stars: 3 }
+}, x => x.stars === 2), "noma"))
