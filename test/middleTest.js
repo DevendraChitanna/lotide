fea7885b7160608f0a97e4712b92f790b45e717b
DevendraChitanna <devendrachitanna@gmail.com>
2021-11-29 17:06:19 -0500

    used mocha and chai for head, tail, and middle test files

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,5 +1,13 @@
+const assert = require('chai').assert;
 const eqArrays = require('../eqArrays')
 const assertArraysEqual = require('../assertArraysEqual')
 const middle = require('../middle')
 
-console.log(assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7, 8, 9]), [5]))
+
+describe("#middle", () => {
+  it("shoud return [5] for [1, 2, 3, 4, 5, 6, 7, 8, 9]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6, 7, 8, 9]), [5])
+  })
+})
+
+// console.log(assertArraysEqual(middle([1, 2, 3, 4, 5, 6, 7, 8, 9]), [5]))
